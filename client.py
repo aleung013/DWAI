585fc0a1b670f8aec82d0462689664d5427c47a7
Vivian <vivianvivianvivianv@gmail.com>
2014-06-03 12:50:45 -0400

    Basic Chat with Python Sockets for terminal

---
+++ client.py
@@ -0,0 +1,42 @@
+import socket, select, string, sys
+
+def prompt():
+    sys.stdout.write('<You> ')
+    sys.stdout.flush()
+
+if __name__ == "__main__":
+    #if len(sys.argv) < 3:
+    #    print 'Usage : python telnet.py hostname port'
+    #    sys.exit()
+
+    host = "149.89.150.121"
+    port = 5000
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+
+    try:
+        s.connect((host,port))
+    except:
+        print 'Unable to connect'
+        sys.exit()
+
+    print 'Connected to remote host. Start sending messages'
+    prompt()
+
+    while True:
+        socket_list = [sys.stdin, s]
+        read_sockets,write_sockets,error_sockets = select.select(socket_list,[],[])
+        for sock in read_sockets:
+            if sock == s:
+                data = sock.recv(4096)
+                if not data:
+                    print '\nDisconnected from chat server'
+                    sys.exit()
+                else:
+                    sys.stdout.write(data)
+                    prompt()
+            else:
+                msg = sys.stdin.readline()
+                s.send(msg)
+                prompt()
+
